071eedba06d834de5bfd611cab5114c00c616804
Daksh <dakshjain48@gmail.com>
2021-04-06 03:15:33 +0530

    updated the python script

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ num_ip = int(input("Number of Instances for which to enable passwordless ssh: ")
 ips = []
 
 for i in range(num_ip):
-    ip = input("Please enter IP #%s: " % (str(i+1)))
+    ip = input("Please enter IP #%d: " % (i+1))
     ips.append(ip)
 
 pub = input("Please enter the contents of your authentication public key (in ~/.ssh/): ")
@@ -14,8 +14,7 @@ pub = input("Please enter the contents of your authentication public key (in ~/.
 to_write = []
 to_write.append('#!/bin/bash\n\n')
 
-for i in range(num_ip):
-    ip = ips[i]
+for ip in ips:
     to_write.append("ssh -i %s ec2-user@%s 'echo \"%s\" >> ~/.ssh/authorized_keys'\n" % (kp_name, ip, pub))
     to_write.append("ssh -i %s ec2-user@%s 'sudo chmod -R 700 ~/.ssh'\n" % (kp_name, ip))
     to_write.append("ssh -i %s ec2-user@%s 'sudo chmod 600 ~/.ssh/*'\n\n" % (kp_name, ip))
