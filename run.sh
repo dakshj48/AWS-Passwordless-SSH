4116b9259d5aa35dccf32738c8e680214e603bd7
Daksh Jain <dakshjain48@gmail.com>
2021-04-30 10:15:16 +0000

    added a confirmation line

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ python3 setup.py
 
 echo "Running the deploy script to enable passwordless SSH on the remote servers."
 bash deploy.sh
+
+echo "Enabled passwordless SSH on all the provided EC2 instances"
